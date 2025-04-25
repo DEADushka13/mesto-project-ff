7b0a035e7f42f6054391b38ba7cc91de38ec630b
Timofeev <timofeev.timofeev1995@yandex.ru>
2025-04-25 16:06:47 +0300

    add profile-edit

---
+++ src/components/profile-edit.js
@@ -0,0 +1,29 @@
+// const edit_form = document.forms.edit_profile;
+// edit_form.elements.name.value = pofile_name;
+// edit_form.elements.description.value = profile_disc;
+// console.log(document.forms.edit_profile);
+// console.log(document.forms.edit_profile.elements);
+
+// Находим форму в DOM
+const formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = document.querySelector(".popup__input_type_name"); // Воспользуйтесь инструментом .querySelector()
+const jobInput = document.querySelector(".popup__input_type_description"); // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", handleFormSubmit);
