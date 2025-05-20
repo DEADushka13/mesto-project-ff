51a554052ffce75378edb5a621763acfca1ab14f
Timofeev Yury <timofeev.timofeev1995@yandex.ru>
2025-05-20 14:46:44 +0300

    add edit profile

--- src/components/open-popup.js
+++ src/components/open-popup.js
@@ -1,5 +1,5 @@
 // Открытие модальных окон
-import { close_withinclick,close_esc } from "./close-popup.js";
+import { close_withinclick, close_esc } from "./close-popup.js";
 const profile = document.querySelector(".profile");
 const places = document.querySelector(".places");
 const new_card_popup = document.querySelector(".popup_type_new-card");
@@ -9,15 +9,22 @@ const edit_popup = document.querySelector(".popup_type_edit");
 profile.addEventListener("click", function (evt) {
   if (evt.target.classList.contains("profile__add-button")) {
     new_card_popup.classList.add("popup_is-opened");
+    //-------------PROBLEM-------------------
     close_withinclick(new_card_popup);
     close_esc(new_card_popup);
+    //-------------PROBLEM-------------------
     // alert("Ураа");
     // add_listener_esc(new_card_popup);
   }
   if (evt.target.classList.contains("profile__edit-button")) {
     edit_popup.classList.add("popup_is-opened");
-    close_withinclick(edit_popup);
-    close_esc(edit_popup);
+    // const edit_form = document.forms.edit_profile;
+    // edit_form.elements.name.value = document.querySelector(".profile__title").textContent;
+    // edit_form.elements.description.value = document.querySelector(".profile__description").textContent;
+    //-------------PROBLEM-------------------
+    //  close_withinclick(edit_popup);
+    // close_esc(edit_popup);
+    //-------------PROBLEM-------------------
     // alert("Ураа");
     // add_listener_esc(edit_popup);
   }
--- src/components/profile-edit.js
+++ src/components/profile-edit.js
@@ -4,8 +4,23 @@
 // console.log(document.forms.edit_profile);
 // console.log(document.forms.edit_profile.elements);
 
+//-------------------------------------------------------
+// Берём значения в форму
+const edit_form = document.forms.edit_profile;
+edit_form.elements.name.value =
+  document.querySelector(".profile__title").textContent;
+edit_form.elements.description.value = document.querySelector(
+  ".profile__description"
+).textContent;
+//-------------------------------------------------------
+
+// const edit_input_name = document.forms.edit_profile.name;
+// edit_input_name.addEventListener("keydown", function () {
+//   console.log("Ураааа");
+// });
+
 // Находим форму в DOM
-const formElement = document.querySelector(".popup__form"); // Воспользуйтесь методом querySelector()
+const formElement = document.querySelector(".popup_type_edit"); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
 const nameInput = document.querySelector(".popup__input_type_name"); // Воспользуйтесь инструментом .querySelector()
 const jobInput = document.querySelector(".popup__input_type_description"); // Воспользуйтесь инструментом .querySelector()
@@ -20,8 +35,11 @@ function handleFormSubmit(evt) {
   // Получите значение полей jobInput и nameInput из свойства value
 
   // Выберите элементы, куда должны быть вставлены значения полей
-
+  document.querySelector(".profile__title").textContent = nameInput.value;
+  document.querySelector(".profile__description").textContent = jobInput.value;
   // Вставьте новые значения с помощью textContent
+  console.log("Ураааа");
+  formElement.classList.remove("popup_is-opened");
 }
 
 // Прикрепляем обработчик к форме:
